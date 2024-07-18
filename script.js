b474e182fd3031371938b38cbd46f41d796403a9
Nathan Thomas <thomas.nathanm@gmail.com>
2024-07-18 16:48:16 -0500

    Add all operations to calculator

--- script.js
+++ script.js
@@ -7,24 +7,22 @@ let operator
 
 document.addEventListener("DOMContentLoaded", () => {
   const multiply = function(num1, num2) {
-    num3 = (num1 * num2)
-    result.textContent= num3;
-    return num3
+    result.textContent = (num1 * num2);
   }
 
   const add = function(num1, num2) {
-    result.textContent= (num1 + num2);
+    result.textContent = (num1 + num2);
   }
 
   const divide = function(num1, num2) {
     if (num2 === 0) {
       return alert("To infinity! And beyond!");
     } 
-    return num1 / num2;
+    result.textContent=  num1 / num2;
   }
 
-  const substract = function(num1, num2) {
-    return num1 - num2;
+  const subtract = function(num1, num2) {
+    result.textContent=  num1 - num2;
   }
 
   const wipe = function() {
@@ -39,13 +37,42 @@ document.addEventListener("DOMContentLoaded", () => {
     number = undefined;
   }
 
+  const clearAll = function() {
+    number = undefined;
+    operator = undefined;
+    num1 = undefined;
+    num2 = undefined;
+    updatedNum2 = undefined;
+    result.textContent = "0";
+    wipe();
+  }
+
+  const checkNumber = function(value) {
+    if (isNaN(value)) {
+      alert("Enter a valid number.")
+      clearAll();
+      wipe();
+    } 
+  }
+
+  const handleNum3 = function() {
+    if (number === undefined) {
+      num1 = Math.floor(num3)
+    } else {
+      num1 = Math.floor(number);
+      checkNumber(num1)
+    }
+  }
+
   const input = document.querySelector("#numberInput");
   const addButton = document.querySelector("#add");
+  const subtractButton = document.querySelector("#subtract");
+  const multiplyButton = document.querySelector("#multiply")
+  const divideButton = document.querySelector("#divide")
   const equalButton = document.querySelector("#equal");
   const result = document.querySelector("#result")
   const clear = document.querySelector("#clear")
 
-  
 
   input.addEventListener("input", (event) => {
     if (typeof num1 === "undefined") {
@@ -57,11 +84,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   addButton.addEventListener("click", (event) => {
     operator = "add";
-    if (number === undefined) {
-      num1 = Math.floor(num3)
-    } else {
-      num1 = Math.floor(number);
-    }
+    handleNum3();
+    wipe();
+  });
+
+  subtractButton.addEventListener("click", (event) => {
+    operator = "subtract";
+    handleNum3();
+    wipe();
+  });
+
+  multiplyButton.addEventListener("click", (event) => {
+    operator = "multiply";
+    handleNum3();
+    wipe();
+  });
+
+  divideButton.addEventListener("click", (event) => {
+    operator = "divide";
+    handleNum3();
     wipe();
   });
 
@@ -70,26 +111,20 @@ document.addEventListener("DOMContentLoaded", () => {
     switch (operator) {
       case "add":
         add(num1, updatedNum2);
-        wipe();
-        cleanup();
         break;
       case "subtract":
         subtract(num1, updatedNum2);
         break;
+      case "multiply":
+        multiply(num1, updatedNum2);
+        break;
+      case "divide":
+        divide(num1, updatedNum2);
+        break;
     }
+    wipe();
+    cleanup();
   });
 
-  clear.addEventListener("click", (event) => {
-    number = undefined;
-    operator = undefined;
-    num1 = undefined;
-    num2 = undefined;
-    updatedNum2 = undefined;
-    result.textContent = "0";
-    wipe();
-  })
+  clear.addEventListener("click", (clearAll))  
 });
-
-
-
-
